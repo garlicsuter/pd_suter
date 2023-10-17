949762ce0a444b0c5ccba04528a0da4888c35d1f
Cleme26 <26cleme@elida.k12.oh.us>
2023-10-17 10:10:32 -0400

    He chases me.

--- Assets/MiniGameSuter/Scripts-Suter/PlayerController2.cs
+++ Assets/MiniGameSuter/Scripts-Suter/PlayerController2.cs
@@ -2,24 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController2 : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    private int count;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    public void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
         
     }
 }
